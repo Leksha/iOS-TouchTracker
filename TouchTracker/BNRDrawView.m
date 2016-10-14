79498c3c9452cab0891a4ca529b1b4b2412bd804
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-14 16:41:44 -0400

    Added drawRect and strokeLine in BNRDrawView

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -7,15 +7,49 @@
 //
 
 #import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDrawView
 
-/*
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line {
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    // Draw finished lines in black
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    if (self.currentLine) {
+        // If there is a line currently being drawn, do it in red
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
 }
-*/
 
 @end
